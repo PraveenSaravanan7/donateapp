640ec48a8c5a39a6bf1232a9aa7830a9a468e914
PraveenSaravanan <61940883+PraveenSaravanan7@users.noreply.github.com>
2020-12-29 14:53:34 +0000

    api set

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const ls=require("local-storage")
 var accessToken=ls("accessToken")
 export default axios.create({
   //baseURL: `http://localhost:3000/`,
-  baseURL: `https://lit-scrubland-88770.herokuapp.com/`,
+  baseURL: `https://blooming-retreat-98193.herokuapp.com/`,
   timeout:3000,
   headers:{ 'Authorization': 'Bearer '+accessToken}
 });
