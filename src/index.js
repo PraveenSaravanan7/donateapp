986de2678edf85f85884810e2d2965b6dbb3cf2e
PraveenSaravanan <61940883+PraveenSaravanan7@users.noreply.github.com>
2020-12-29 15:39:23 +0000

    hashrouter

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import 'jquery/dist/jquery.slim'
 import 'bootstrap/dist/js/bootstrap';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router>
     <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
