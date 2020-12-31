2912b9577eab8a5f209b7c7f3015ce4651e4d85b
PraveenSaravanan <61940883+PraveenSaravanan7@users.noreply.github.com>
2020-12-31 09:05:22 +0000

    all set

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import "./Post.css";
 import FeatherIcon from "feather-icons-react";
 import { Link } from 'react-router-dom';
+import  axios from "../api";
 const copy = require('clipboard-copy')
- 
+const ls=require("local-storage")
 export const Post = ({data,setcopied,fullview}) => {
     var [showcontact,setshowcontact]=useState(false)
+    var [deleted,setdeleted]=useState(false)
     function copylink(){
         if(window.location.port){
         var text=window.location.hostname+":"+window.location.port+"/post/"+data._id}
@@ -20,7 +22,20 @@ export const Post = ({data,setcopied,fullview}) => {
         setcopied(text)
         //console.log(text)
     }
-    return (
+    
+    async function DeletePost(pid,photo) {
+        try {   
+            var url="/posts?pid="+pid    
+          
+          const response = await axios.delete(url);         
+          console.log(response.data);
+          setdeleted(true)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    return (<>
+    {!deleted &&
         <div className="post-div" >
            
             <Link to={"/profile/"+data.userid._id} >
@@ -31,12 +46,15 @@ export const Post = ({data,setcopied,fullview}) => {
             </Link>
            
           
-            <div class="dropdown float-right">
+            <div className="dropdown float-right">
   <button className="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
   <FeatherIcon icon="chevron-down"   ></FeatherIcon>
   </button>
   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a className="dropdown-item text-danger" >Delete</a>
+      {ls("user_id")==data.userid._id?
+    <a className="dropdown-item text-danger" onClick={()=>DeletePost(data._id,data.photo)} >Delete</a> :
+    <a className="dropdown-item text-dark"  >Report  </a>
+}
   </div>
 </div>
             { !fullview?  <Link to={"/post/"+data._id} >
@@ -67,26 +85,27 @@ export const Post = ({data,setcopied,fullview}) => {
                <span><b>Address:</b> {data.address}, {data.state}, {data.country} </span>
            </div>}
 
-           <div className="row text-primary mt-3 fs16 " >
+           <div className="row text-primary mt-2 fs16 " >
                <div className="col text-center px-0 unselectable" onClick={()=>setshowcontact(true)} >
-                   <span ><b>
+                   <span className="btn p-0 m-0 text-primary " ><b>
                    <FeatherIcon  icon="phone" size={15} ></FeatherIcon> Contact
                    </b></span>
                </div>
                <Link to={"/post/"+data._id} >
                <div className="col text-center px-0 unselectable"  >
-                   <span ><b>
+                   <span className="btn p-0 m-0 text-primary " ><b>
                    <FeatherIcon  icon="message-circle" size={15} ></FeatherIcon> Comment
                    </b></span>
                </div>      
                 </Link>  
-            <div className="col text-center px-0 unselectable" onClick={()=>{ ()=> copylink()}} >
-                   <span ><b>
+            <div className="col text-center px-0 unselectable" onClick={()=>{copylink()}} >
+                   <span className="btn p-0 m-0 text-primary " ><b>
                    <FeatherIcon  icon="share-2" size={15} ></FeatherIcon> Share
                    </b></span>
               
                </div>
            </div>
         </div>
+    }</>
     )
 }
