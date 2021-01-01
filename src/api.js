c4aa8aee0d381e99f5aed3c3d4ecdae5f1b54216
PraveenSaravanan <61940883+PraveenSaravanan7@users.noreply.github.com>
2021-01-01 13:15:00 +0000

    done

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ var accessToken=ls("accessToken")
 export default axios.create({
   //baseURL: `http://localhost:8080/`,
   //baseURL: `https://blooming-retreat-98193.herokuapp.com/`,
-  baseURL: `https://aqueous-headland-10653.herokuapp.com/`,
+    baseURL: `https://aqueous-headland-10653.herokuapp.com/`,
   timeout:3000,
   headers:{ 'Authorization': 'Bearer '+accessToken}
 });
