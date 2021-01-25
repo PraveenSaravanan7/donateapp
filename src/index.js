293f64ea780a196efea249aad3fab0e6fb657323
PraveenSaravanan <61940883+PraveenSaravanan7@users.noreply.github.com>
2021-01-25 07:22:20 +0000

    commit final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Bootstraps-custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.slim'
 import 'bootstrap/dist/js/bootstrap';
 import './index.css';
